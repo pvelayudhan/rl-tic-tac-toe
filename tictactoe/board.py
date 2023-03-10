66cb1f375855ec643a9eafb841521b1efd32b6ec
Prashanth Velayudhan <psvelayu@gmail.com>
2023-03-10 09:57:01 -0500

    add example uses of Board class

--- tictactoe/board.py
+++ tictactoe/board.py
@@ -23,14 +23,6 @@ class Board:
     def play(self, location, token):
         self.squares[location] = token
 
-    #def show(self):
-    #    print("")
-    #    print(f"3 | {self.squares['a3']}   {self.squares['b3']}   {self.squares['c3']}")
-    #    print(f"2 | {self.squares['a2']}   {self.squares['b2']}   {self.squares['c2']}")
-    #    print(f"1 | {self.squares['a1']}   {self.squares['b1']}   {self.squares['c1']}")
-    #    print("    ---------")
-    #    print("    a   b   c")
-
 
     def show(self):
         print("")
@@ -41,13 +33,15 @@ class Board:
         print("    a   b   c")
 
 
-
+# Initialize board
 board = Board()
 
-
-
-
-#|%%--%%| <HrfwsSBxmj|iQV9CE4VPG>
-
-
+# Display board
 board.show()
+
+# Play a move
+board.play("a3", "x")
+board.show()
+
+# Information about a particular square
+board.squares["a2"]
