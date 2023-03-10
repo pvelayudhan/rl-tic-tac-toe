993cac7a9f50c3d693a433c878e4fc627017f2af
Prashanth Velayudhan <psvelayu@gmail.com>
2023-03-09 21:11:01 -0500

    first commit

---
+++ tictactoe/board.py
@@ -0,0 +1,53 @@
+class Board:
+
+    """
+    Board:
+    a3 b3 c3
+    a2 b2 c2
+    a1 b1 c1
+    """
+    def __init__(self):
+        self.squares = {
+            "a1": ".",
+            "b1": ".",
+            "c1": ".",
+            "a2": ".",
+            "b2": ".",
+            "c2": ".",
+            "a3": ".",
+            "b3": ".",
+            "c3": "."
+        }
+
+
+    def play(self, location, token):
+        self.squares[location] = token
+
+    #def show(self):
+    #    print("")
+    #    print(f"3 | {self.squares['a3']}   {self.squares['b3']}   {self.squares['c3']}")
+    #    print(f"2 | {self.squares['a2']}   {self.squares['b2']}   {self.squares['c2']}")
+    #    print(f"1 | {self.squares['a1']}   {self.squares['b1']}   {self.squares['c1']}")
+    #    print("    ---------")
+    #    print("    a   b   c")
+
+
+    def show(self):
+        print("")
+        print(f"3 | {self.squares['a3']}   {self.squares['b3']}   {self.squares['c3']}")
+        print(f"2 | {self.squares['a2']}   {self.squares['b2']}   {self.squares['c2']}")
+        print(f"1 | {self.squares['a1']}   {self.squares['b1']}   {self.squares['c1']}")
+        print("    ---------")
+        print("    a   b   c")
+
+
+
+board = Board()
+
+
+
+
+#|%%--%%| <HrfwsSBxmj|iQV9CE4VPG>
+
+
+board.show()
