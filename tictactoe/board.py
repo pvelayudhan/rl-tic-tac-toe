a2e9d33782ef5794ecfa8d47de9c68791cd83218
Prashanth Velayudhan <psvelayu@gmail.com>
2023-03-12 22:14:43 -0400

    reformat board printing to not exceed 80 chars per line

--- tictactoe/board.py
+++ tictactoe/board.py
@@ -19,29 +19,25 @@ class Board:
             "c3": "."
         }
 
-
     def play(self, location, token):
         self.squares[location] = token
 
-
     def show(self):
-        print("")
-        print(f"3 | {self.squares['a3']}   {self.squares['b3']}   {self.squares['c3']}")
-        print(f"2 | {self.squares['a2']}   {self.squares['b2']}   {self.squares['c2']}")
-        print(f"1 | {self.squares['a1']}   {self.squares['b1']}   {self.squares['c1']}")
+        rank3 = ("3 | ") + \
+            str(self.squares['a3']) + "   " + \
+            str(self.squares['b3']) + "   " + \
+            str(self.squares['c3'])
+        rank2 = ("2 | ") + \
+            str(self.squares['a2']) + "   " + \
+            str(self.squares['b2']) + "   " + \
+            str(self.squares['c2'])
+        rank1 = ("1 | ") + \
+            str(self.squares['a1']) + "   " + \
+            str(self.squares['b1']) + "   " + \
+            str(self.squares['c1'])
+        print(rank3)
+        print(rank2)
+        print(rank1)
         print("    ---------")
         print("    a   b   c")
-
-
-# Initialize board
-board = Board()
-
-# Display board
-board.show()
-
-# Play a move
-board.play("a3", "x")
-board.show()
-
-# Information about a particular square
-board.squares["a2"]
+        print("\n")
