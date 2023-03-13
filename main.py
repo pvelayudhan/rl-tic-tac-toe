528a48f9ede9c5571b91872d07bc4b5f572c474b
Prashanth Velayudhan <psvelayu@gmail.com>
2023-03-12 22:14:16 -0400

    move main code out of board class file and into main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-import tictactoe
+import tictactoe.board as tb
+
+# Initialize board
+board = tb.Board()
+
+
+# Display board
+board.show()
+
+# Play a move
+board.play("a3", "x")
+board.show()
+
+# Information about a particular square
+board.squares["a2"]
