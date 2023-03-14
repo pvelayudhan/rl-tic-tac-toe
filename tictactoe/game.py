b1b8ec8f1c845af9dda697bc7e0ee3ce76ecb62b
Prashanth Velayudhan <psvelayu@gmail.com>
2023-03-13 23:52:17 -0400

    exploring game class

---
+++ tictactoe/game.py
@@ -0,0 +1,17 @@
+from tictactoe.board import Board
+import numpy as np
+
+
+class Game:
+
+    def __init__(self):
+        self.board = Board()
+
+
+
+#a = Game()
+#
+#print(a.board.squares)
+#
+#np.prod(a.board.squares)
+
