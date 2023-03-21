57c7c0ce4d224b421dd6373e17e100f05145b864
Prashanth Velayudhan <psvelayu@gmail.com>
2023-03-21 00:35:08 -0400

    adapting homl3 play_one_step function

--- policies.py
+++ policies.py
@@ -64,12 +64,33 @@ game.play(2, 0)
 def play_one_move(game, model, loss_fn):
     with tf.GradientTape() as tape:
         formatted_board = game.board.squares.reshape(1, 9)
+        # 1. Get a prediction from model()
         square_probs = model(formatted_board)
-        # have a function here to remove illegal moves
-        # have a function here to probabilistically choose an action
+        # 2. Pick an action that can be fed into environment
+        target_square = choose_move(game, square_probs)
+        # Switch the above^ to pull out formatted move as a square, add
+        #  a new function that makes square into rank and file
+        # Next things to do
         loss = tf.reduce_mean(loss_fn(target_square, square_probs))
     grads = tape.gradient(loss, model.trainable_variables)
-    # function here to convert action into rank and file
-    rank, file = square_probs_to_action(square_probs)
-    state, reward, done = game.play(rank, file)
-    return state, reward, done, grads
+    target_square = np.reshape(target_square, [3, 3])
+    rank, file = np.where(target_square == 1)
+    reward, done = game.play(rank[0], file[0])
+    return reward, done, grads
+
+
+# The HOML3 function
+def play_one_step(env, obs, model, loss_fn):
+    with tf.GradientTape() as tape:
+        # 1. (DONE) Get a prediction from model()
+        left_proba = model(obs[np.newaxis])
+        # Generate one one-sized array of uniform numbers and compare with left
+        #  probability. The action is false if action is false
+        # 2. (DONE) Pick an action that can be fed into environment
+        action = (tf.random.uniform([1, 1]) > left_proba)
+        # If the action is false (0), the target probability of going left is 1
+        y_target = tf.constant([[1.]]) - tf.cast(action, tf.float32)
+        loss = tf.reduce_mean(loss_fn(y_target, left_proba))
+    grads = tape.gradient(loss, model.trainable_variables)
+    obs, reward, done, truncated, _ = env.step(int(action))
+    return obs, reward, done, truncated, grads
