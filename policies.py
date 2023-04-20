e388730c10ef5a84a914700862d523bfafad526d
Prashanth Velayudhan <psvelayu@gmail.com>
2023-04-19 22:44:47 -0400

    add remaining homl functions

--- policies.py
+++ policies.py
@@ -78,6 +78,7 @@ def play_one_move(game, model, loss_fn):
         square_probs = model(formatted_board)
         # 3. Pick an action that can be fed into environment
         target_square = choose_move(game, square_probs)
+        #print(square_probs)
         # 4. Define the loss(?)
         loss = tf.reduce_mean(loss_fn(target_square, square_probs))
     # Calculate the gradients between predicted and actual(?)
@@ -105,11 +106,12 @@ def play_one_step(env, obs, model, loss_fn):
     obs, reward, done, truncated, _ = env.step(int(action))
     return obs, reward, done, truncated, grads
 
-#|%%--%%| <6rPK8eNKTp|wrtgaKWxPI>
+#|%%--%%| <6rPK8eNKTp|SzXI15gBzx>
 
 
 loss_fn = tf.keras.losses.categorical_crossentropy
-loss_fn = tf.keras.losses.binary_crossentropy
+
+# loss_fn = tf.keras.losses.binary_crossentropy
 
 
 # y = mx0 + b -> y' = m = -3
@@ -143,6 +145,138 @@ y = mx + b
 Repeat!
 
 """
+#|%%--%%| <SzXI15gBzx|1sGtfgIBy2>
 
+# Each step = one turn of tic-tac-toe
+# Each "episode" = one whole game of tic-tac-toe
+
+
+game = Game()
+
+def play_multiple_episodes(game, n_episodes, model, loss_fn, n_max_steps=9):
+    all_rewards = []
+    all_grads = []
+    #1. Loop through many games of ttt
+    for episode in range(n_episodes):
+        current_rewards = []
+        current_grads = []
+        #2. Initialize a new game
+        #obs, _ = env.reset()
+        #print(f"Playing game #{episode}")
+        game = Game()
+        for _ in range(n_max_steps):
+            #obs, reward, done, truncated, grads = play_one_step(env, obs, model, loss_fn)
+            reward, done, grads = play_one_move(game, model, loss_fn)
+            current_rewards.append(reward)
+            current_grads.append(grads)
+            if done:
+                break
+        all_rewards.append(current_rewards)
+        all_grads.append(current_grads)
+    # all_rewards is a list containing a list of rewards for each episode
+    # all_grads is a list containing a list of gradients for each episode
+    return all_rewards, all_grads
+
+
+# A discounted reward is the reward for the current step plus the reward for
+#  all future steps multiplied by the disocunt factor. For example, if our
+#  rewards were [2, 4, 8] and we had a discount factor of 0.5, our discounted
+#  rewards would be [(2 + 8*0.5 =) 6, (4 + 8*0.5 =) 8, 8]. Note that this
+#  calculation is done starting at the last reward and working backwards.
+def discount_rewards(rewards, discount_factor):
+    discounted = np.array(rewards)
+    # Range params are start index, end index, and step size
+    for step in range(len(rewards) - 2, -1, -1):
+        discounted[step] += discount_factor * discounted[step + 1]
+    return discounted
+
+
+# This function performs standard normalization across ALL rewards
+def discount_and_normalize_rewards(all_rewards, discount_factor):
+    all_discounted_rewards = [discount_rewards(rewards, discount_factor)
+                              for rewards in all_rewards]
+    flat_rewards = np.concatenate(all_discounted_rewards)
+    reward_mean = flat_rewards.mean()
+    reward_std = flat_rewards.std()
+    return [(discounted_rewards - reward_mean) / reward_std
+            for discounted_rewards in all_discounted_rewards]
+
+
+
+n_iterations = 10
+n_episodes_per_update = 2
+discount_factor = 0.95
+
+
+#|%%--%%| <1sGtfgIBy2|kbUk7tZ9l2>
+
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Dense(5, activation="relu"),
+    tf.keras.layers.Dense(9, activation="sigmoid")
+])
+
+optimizer = tf.keras.optimizers.Nadam(learning_rate=0.01)
+
+#|%%--%%| <kbUk7tZ9l2|lCUWqS6p3n>
+
+
+
+for iteration in range(n_iterations):
+    print(f"{iteration + 1} / {n_iterations}")
+    all_rewards, all_grads = play_multiple_episodes(game, n_episodes_per_update, model, loss_fn)
+    # extra code – displays some debug info during training
+    total_rewards = sum(map(sum, all_rewards))
+    print(f"\rIteration: {iteration + 1}/{n_iterations},"
+          f" mean rewards: {total_rewards / n_episodes_per_update:.1f}", end="")
+
+    all_final_rewards = discount_and_normalize_rewards(all_rewards,
+                                                       discount_factor)
+    all_mean_grads = []
+    for var_index in range(len(model.trainable_variables)):
+        mean_grads = tf.reduce_mean(
+            [final_reward * all_grads[episode_index][step][var_index]
+             for episode_index, final_rewards in enumerate(all_final_rewards)
+                 for step, final_reward in enumerate(final_rewards)], axis=0)
+        all_mean_grads.append(mean_grads)
+    optimizer.apply_gradients(zip(all_mean_grads, model.trainable_variables))
+
+#|%%--%%| <lCUWqS6p3n|dFrmdtjX0e>
+
+
+
+game = Game()
+
+game.board.show()
+
+play_one_move(game, model, loss_fn)
+
+game.board.show()
+
+game.play(1, 1)
+
+game.board.show()
+
+play_one_move(game, model, loss_fn)
+
+game.board.show()
+
+game.play(2, 2)
+
+play_one_move(game, model, loss_fn)
+
+game.board.show()
+
+game.play(2, 1)
+
+play_one_move(game, model, loss_fn)
+
+game.board.show()
+
+
+game.play(0, 2)
+
+
+play_one_move(game, model, loss_fn)
 
 
