5af52228d859faa8d26fdd4241a2f494fc258497
Prashanth Velayudhan <psvelayu@gmail.com>
2023-08-09 10:41:09 -0400

    reimplementiung fcarsten repo

--- 01_basics.py
+++ 01_basics.py
@@ -1,9 +1,5 @@
-# Re-implementation of https://github.com/fcarsten/tic-tac-toe/blob/master/Part%201%20-%20Computer%20Tic%20Tac%20Toe%20Basics.ipynb
+# Re-implementation of github.com/fcarsten/tic-tac-toe
 
 from tictactoe import Game
-import tensorflow as tf
-import numpy as np
-import random
-import matplotlib.pyplot as plt
 
 game = Game()
