1dc6ecdc833efd78dfa5b517f3cfbdd1b0f99693
Prashanth Velayudhan <psvelayu@gmail.com>
2023-09-12 21:42:52 -0400

    replicated chapter 1

--- 01_basics.py
+++ 01_basics.py
@@ -2,4 +2,27 @@
 
 from tictactoe import Game
 
-game = Game()
+
+outcomes = []
+
+for i in range(100000):
+    print(i)
+    game = Game()
+    while not game.done:
+        game.play_random_move()
+    outcomes.append(game.winner)
+
+n_draws = outcomes.count(None)
+n_p1_win = outcomes.count(1)
+n_p2_win = outcomes.count(2)
+print(n_draws / len(outcomes))
+print(n_p1_win / len(outcomes))
+print(n_p2_win / len(outcomes))
+
+"""
+12.5% draw
+58.6% P1 win
+28.9% P2 win
+"""
+
+# 12.5%
--- tictactoe.py
+++ tictactoe.py
@@ -1,4 +1,6 @@
 import numpy as np
+import tensorflow as tf
+import random
 
 
 class Board:
@@ -100,3 +102,21 @@ class Game:
         # Update the turn
         self.turn = self.turn + 1
         return None
+
+    def play_random_move(self):
+        if self.done is True:
+            return None
+        legal_moves = np.where(np.reshape(self.squares, [9]) == 0)[0]
+        legal_move_chosen = False
+        while legal_move_chosen is False:
+            chosen_move = np.random.choice(np.arange(9), 1)[0]
+            if chosen_move in legal_moves:
+                legal_move_chosen = True
+        formatted_move = np.zeros(9, dtype=float)
+        formatted_move[chosen_move] = 1
+        formatted_move = tf.convert_to_tensor(formatted_move[np.newaxis])
+        target_square = np.reshape(formatted_move, [3, 3])
+        rank, file = np.where(target_square == 1)
+        self.play(rank[0], file[0])
+        return None
+
